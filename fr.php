5c1757a099bdb1b634f4d951316dec68bb115266
Hervé H <nenes25@users.noreply.github.com>
2024-04-23 09:42:48 +0200

    Delete fr.php

--- fr.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{hhpsmigrationupgradedb}prestashop>hhpsmigrationupgradedb_4c52899a9d47a4882a36445cc86d18fe'] = 'Hh migration upgrade db';
-$_MODULE['<{hhpsmigrationupgradedb}prestashop>hhpsmigrationupgradedb_55fa787a1677b4bab0aafa90d0f9b08d'] = 'Simplifiez vos migrations prestashop';
