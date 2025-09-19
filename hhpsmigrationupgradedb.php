2c7446f07416ac021f44bb9136f6ccfbd8757de9
Hervé Hennes <nenes_fr@yahoo.fr>
2025-09-19 13:15:46 +0200

    #12 fix version check

--- hhpsmigrationupgradedb.php
+++ hhpsmigrationupgradedb.php
@@ -28,7 +28,7 @@ class Hhpsmigrationupgradedb extends Module
         $this->author = 'hhennes';
         $this->bootstrap = true;
         $this->dependencies = ['autoupgrade'];
-        $this->ps_versions_compliancy = ['min' => '1.7.5.0', 'max' => '8.2.*'];
+        $this->ps_versions_compliancy = ['min' => '1.7.5.0', 'max' => '8.3'];
         parent::__construct();
 
         $this->displayName = $this->l('Hh migration upgrade db');
